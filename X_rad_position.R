45dc0ee4b542c5f4035f0fefa68ae940e12ff744
seaman <semen.bko@ya.ru>
2016-03-16 21:15:17 +0600

    rad

--- X_rad_position.R
+++ X_rad_position.R
@@ -1,6 +1,6 @@
-ggplot(FISHdata, aes(x=X_efvMax, y=..density.., fill=Sp))+
+ggplot(FISHdata, aes(x=X_efvMax, y=..density..))+
   facet_wrap(~Sp_Tissue)+
-  geom_histogram()+
-  geom_density()
+  geom_histogram(aes(fill=Sp))+
+  geom_density(aes(col=Sp))
 
 table(FISHdata$X_efvMax, FISHdata$Sp_Tissue)
